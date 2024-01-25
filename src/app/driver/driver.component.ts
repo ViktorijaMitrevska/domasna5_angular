f4147d0940264e1851b314cf1252ad015e1ce6ce
ViktorijaMitrevska <mitrevskaviktorija4@gmail.com>
2024-01-25 22:44:46 +0100

    domasna5

---
+++ src/app/driver/driver.component.ts
@@ -0,0 +1,52 @@
+import { Component, EventEmitter, Input, OnInit,Output} from '@angular/core';
+import { Driver } from'../module/klasa';
+import {CommonModule} from '@angular/common';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-driver',
+  standalone: true,
+  imports: [CommonModule,NgClass],
+  templateUrl: './driver.component.html',
+  styleUrl: './driver.component.css'
+})
+export class DriverComponent implements OnInit {
+  constructor(){}
+  @Input()
+  ime:String="";
+  @Input()
+  vozac:Driver={} as Driver;
+  @Input()
+  reden:Number={} as Number;
+  @Output()
+  shofer:Driver={} as Driver;
+  @Output()
+  cuci=new EventEmitter<any>();
+
+  klasi(){
+    return{'poz':this.vozac.category=="BEGINER",
+            'poz1':this.vozac.category=="ADVANCED",
+          'course-card':true}
+  }
+  klik(){
+    var link=this.vozac.iconUrl;
+    window.open(link,"_blank");
+    console.log("me klikna");
+    this.cuci.emit(this.vozac);
+  }
+  stil(){
+    if(this.vozac.category=="ADVANCED"){
+      return {'background-color':'red','font-size':'20px','color':'black'}
+    }
+    else if(this.vozac.category=="BEGINER"){
+      return{'background-color':'blue','font-size':'20px','color':'black'}
+    }
+    else{
+      return{'background-color':'yellow','font-size':'20px','color':'black'}
+    }
+  }
+  ngOnInit(): void {
+    
+  }
+
+}
---
+++ src/db-data.ts
@@ -0,0 +1,37 @@
+
+
+export const DRIVERS: any = [
+
+   // undefined,
+
+    {
+        id: 1,
+        name:"Max Vertappen",
+        iconUrl: "https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/verstappen.jpg.img.1920.medium.jpg/1701270073824.jpg",
+        team:" Red Bull Racing",
+        category: 'ADVANCED',
+        points:500,
+    },
+    {
+        id: 2,
+        name: "Lewis Hmilton",
+        iconUrl: "https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/hamilton.jpg.img.1920.medium.jpg/1677069594164.jpg",
+        team:"Mercedes",
+        category: 'ADVANCED',
+        points:450,
+    },
+
+    {
+        id: 3,
+        name: "Sergio Perez",
+        iconUrl:"https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/perez.jpg.img.1920.medium.jpg/1677069773437.jpg",
+        team: "Red Bull",
+        category: 'BEGINER',
+        points:320,
+    },
+
+    
+
+];
+
+
